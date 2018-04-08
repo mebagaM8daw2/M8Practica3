10f156ec1bfeddde71e11d5730dc64d9c47e76ab
Meritxell <mebagam8daw2@gmail.com>
2018-04-08 22:13:48 +0200

    Primera versiÃ³

---
+++ Hola.java
@@ -0,0 +1,12 @@
+/**
+* @author X (on X és el teu nom d'usuari Github)
+*/
+import java.io.*;
+public class Hola {
+    /**
+    * @param args the command line arguments
+    */
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hola Món"); 
+    }
+}
